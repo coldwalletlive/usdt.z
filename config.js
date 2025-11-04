44c95802ed5632b3ddea3a2ec4243ecd589c4767
landing page <yasin.oky2025@gmail.com>
2025-11-04 12:34:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-script
-  window.USDTZ_ADDRESS = 0xee9aa8b56cbc6074558eb7b8860242264590a095;
-  window.CONTACT_EMAIL = contact@usdtz.app;
-  window.SITE_URL      = httpsusdtz.app;
-script
+window.USDTZ_ADDRESS = "0xee9aa8b56cbc6074558eb7b8860242264590a095";
+window.CONTACT_EMAIL = "contact@usdtz.app";
+window.SITE_URL = "https://usdtz.app";
+window.LOGO_URL = "https://s2.coinmarketcap.com/static/img/coins/200x200/38134.png";
